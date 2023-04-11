14158595a72c7464d86c4949e2b42b457acdac8c
felipe <gael.ferreira505@gmail.com>
2023-04-10 22:01:15 -0300

    novo

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {InputText} from './components/InputText'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>'Olá, mundo!'</h1>
+
+      <InputText />
+      <input type="text" name="nome" id="nome" placeholder="nome"/>
+      <br/>
+      <input type="text" name="sobrenome" id="sobrenome" placeholder="sobrenome"/>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/InputText.jsx
@@ -0,0 +1,7 @@
+export function InputText() {
+    return(
+        <input type="text" name="nome" id="nome" placeholder="nome"/>
+    );
+}
+
+//export default InputText
