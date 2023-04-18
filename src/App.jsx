7d994c85a2932632566158ca397127e4fbb26c06
felipe <gael.matos505@gmail.com>
2023-04-17 21:48:16 -0300

    branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,16 @@
 import { useState } from 'react'
-import './App.css'
+// import './App.css'
 import {InputText} from './components/InputText'
 import { MyTitle } from './components/MyTitle'
+import { MyNavbar } from './components/MyNavbar';
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
-      {/* <h1>'Olá! Meu nome é Larissa'</h1> */}
-      < MyTitle title = 'Novo título' />
-
-      <InputText 
-        id='nome' 
-        name='nome'
-        placeholder='nome'
-      />
-      <br/>
-      <InputText 
-        id='sobrenome'
-        name='sobrenome'
-        placeholder='sobrenome'
-      />
+    <MyNavbar />
     </>
-  )
+  );
 }
 
 export default App
